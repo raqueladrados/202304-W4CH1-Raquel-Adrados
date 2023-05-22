46760ca86724a0b02d55936246cca948c7aa926d
Raquel Adrados <radrados00@gmail.com>
2023-05-22 18:48:19 +0200

    Add changes in components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import { App } from "./components/app";
 
 ReactDOM.createRoot(
   document.getElementById(".container") as HTMLElement
 ).render(
   <React.StrictMode>
-    <div />
+    <App />
   </React.StrictMode>
 );
