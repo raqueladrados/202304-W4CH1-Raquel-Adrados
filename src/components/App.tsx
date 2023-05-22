10663dcf0463a82b15882dbc913c68d171326b50
Raquel Adrados <radrados00@gmail.com>
2023-05-22 19:32:46 +0200

    Add button component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from "./Info";
+import { Button } from "./Button";
 
 export function App() {
-  const name = "The pointing gentleman";
+  const name = "The caspers gentleman";
+  const info = "0 caspers pointing at you";
+  const button = "SELECT ALL";
   return (
     <>
       <Header name={name}></Header>
+      <section className="controls">
+        <Button info={info} button={button}></Button>
+      </section>
     </>
   );
 }
