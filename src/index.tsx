363d157a4c44bbd8c7a1f4cd9c605841a5fa6401
Raquel Adrados <radrados00@gmail.com>
2023-05-22 18:14:45 +0200

    Add gentleman mock

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById(".container") as HTMLElement
+).render(
   <React.StrictMode>
     <div />
   </React.StrictMode>
